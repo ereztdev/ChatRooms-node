24605cb3aff130a8d6efd0b57becd63871ef1c36
erezt.dev <erezt.dev@gmail.com>
2019-04-25 17:24:32 +0300

    user entity + room structure, client & message arrays - WIP

--- index.js
+++ index.js
@@ -20,35 +20,45 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var messageHistory = [];
+
+var clientArray = [];
 io.on('connection', socket => {
-  console.log('a user connected');
+  let ip = socket.handshake.address;
+
+  let user = new class User{};
+  user.ip = ip;
+  user.id = Date.now();
+
+  clientArray.push(user)
+
+  console.log('a user connected',user);
+  console.log(clientArray);
 
   socket.on('disconnect', reason => {
-    console.log('user disconnected');
+    console.log('user disconnected', {reason});
   });
   socket.on('new message', data => {
-    console.log('bdsfbdbdb', data);
+    console.log({data});
+    messageHistory.push(data)
+    socket.broadcast.to(data.room).emit('receive message', data)
+    console.log({messageHistory});
   });
-
-
-  // socket.on('room', data => {
-  //   console.log('room join');
-  //   console.log(data);
-  //   socket.join(data.room);
-  // });
-  //
-  // socket.on('leave room', data => {
-  //   console.log('leaving room');
-  //   console.log(data);
-  //   socket.leave(data.room)
-  // });
-  //
-  socket.on('new message', data => {
-    console.log("socket.on('new message', data: ",data.room);
-    socket.broadcast
-      .to(data.room)
-      .emit('receive message', data)
+  socket.on('room', data => {
+    console.log('room join',data);
+    socket.join(data.room);
   });
+  //
+  socket.on('leave room', data => {
+    console.log('leaving room');
+    console.log('leaving room',data);
+    socket.leave(data.room)
+
+  });
+  //
+  // socket.on('new message', data => {
+  //   console.log("new message");
+  //   socket.broadcast.to(data.room).emit('receive message', data)
+  // });
   // socket.on('new message', data => {
   //   console.log(data.room);
   //   socket.broadcast
